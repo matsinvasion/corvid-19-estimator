1da30433d2178b2b63f73577212f746eddfecbef
matsinvasion <matkmusasizi@gmail.com>
2020-04-10 01:33:16 +0300

    hide decimals

--- src/estimator.js
+++ src/estimator.js
@@ -10,11 +10,11 @@ const covid19ImpactEstimator = (data) => {
   estimator.impact.currentlyInfected = data.reportedCases * 10;
   estimator.impact.infectionsByRequestedTime = estimator.impact.currentlyInfected
   * projectedInfections(data);
-  estimator.impact.severeCasesByRequestedTime = estimator.impact.currentlyInfected
-  * projectedInfections(data) * Math.trunc((15 / 100));
-  estimator.impact.hospitalBedsByRequestedTime = estimator.impact.currentlyInfected
-  * projectedInfections(data) * Math.trunc((15 / 100))
-    - ((35 / 100) * data.totalHospitalBeds);
+  estimator.impact.severeCasesByRequestedTime = Math.trunc(estimator.impact.currentlyInfected
+  * projectedInfections(data) * (15 / 100));
+  estimator.impact.hospitalBedsByRequestedTime = Math.trunc(estimator.impact.currentlyInfected
+  * projectedInfections(data) * (15 / 100)
+    - ((35 / 100) * data.totalHospitalBeds));
   estimator.impact.casesForICUByRequestedTime = (5 / 100) * estimator
     .impact
     .infectionsByRequestedTime;
@@ -28,11 +28,11 @@ const covid19ImpactEstimator = (data) => {
   estimator.severeImpact.currentlyInfected = data.reportedCases * 50;
   estimator.severeImpact.infectionsByRequestedTime = estimator.severeImpact.currentlyInfected
    * projectedInfections(data);
-  estimator.impact.severeCasesByRequestedTime = estimator.severeImpact.currentlyInfected
-  * projectedInfections(data) * Math.trunc((15 / 100));
-  estimator.severeImpact.hospitalBedsByRequestedTime = estimator.severeImpact.currentlyInfected
-  * projectedInfections(data) * Math.trunc((15 / 100))
-    - ((35 / 100) * data.totalHospitalBeds);
+  estimator.impact.severeCasesByRequestedTime = Math.trunc(estimator.severeImpact.currentlyInfected
+  * projectedInfections(data) * (15 / 100));
+  estimator.severeImpact.hospitalBedsByRequestedTime = Math.trunc(estimator.severeImpact.currentlyInfected
+  * projectedInfections(data) * (15 / 100)
+    - ((35 / 100) * data.totalHospitalBeds));
   estimator.severeImpact.casesForICUByRequestedTime = (5 / 100) * estimator.severeImpact
     .infectionsByRequestedTime;
   estimator.severeImpact.casesForVentilatorsByRequestedTime = (2 / 100) * estimator
