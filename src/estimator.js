0d40432587c12d3855eed43f76ba38c84ce9cb47
matsinvasion <matkmusasizi@gmail.com>
2020-04-09 19:47:50 +0300

    initial commit- challenge 1 to 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,46 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+//  Estimator
+  const estimator = {};
+  estimator.data = data;
+  estimator.impact = {};
+  estimator.severeImpact = {};
+  estimator.impact.currentlyInfected = data.reportedCases * 10;
+  estimator.impact.infectionsByRequestedTime = estimator.impact.currentlyInfected
+  * (data.timeToElapse / 3);
+  estimator.impact.severeCasesByRequestedTime = estimator
+    .impact
+    .infectionsByRequestedTime * (15 / 100);
+  estimator.impact.hospitalBedsByRequestedTime = estimator.impact.severeCasesByRequestedTime
+   / (data.totalHospitalBeds - ((65 / 100) * data.totalHospitalBeds));
+  estimator.impact.casesForICUByRequestedTime = (5 / 100) * estimator
+    .impact
+    .infectionsByRequestedTime;
+  estimator.severeImpact.casesForICUByRequestedTime = (5 / 100) * estimator
+    .impact
+    .infectionsByRequestedTime;
+  estimator.impact.casesForVentilatorsByRequestedTime = (2 / 100) * estimator.impact;
+  estimator.impact.dollarsInFlight = estimator.impact.infectionsByRequestedTime * data
+    .region.avgDailyIncomePopulation * data
+    .region.avgDailyIncomeInUSD * data.timeToElapse;
+  estimator.severeImpact.currentlyInfected = data.reportedCases * 50;
+  estimator.severeImpact.infectionsByRequestedTime = estimator.severeImpact.currentlyInfected
+   * (data.timeToElapse / 3);
+  estimator.impact.severeCasesByRequestedTime = estimator
+    .severeImpact
+    .infectionsByRequestedTime * (15 / 100);
+  estimator.severeImpact.hospitalBedsByRequestedTime = estimator.impact.severeCasesByRequestedTime
+  / (data.totalHospitalBeds - ((65 / 100) * data.totalHospitalBeds));
+  estimator.severeImpact.casesForICUByRequestedTime = (5 / 100) * estimator.severeImpact
+    .infectionsByRequestedTime;
+  estimator.severeImpact.casesForVentilatorsByRequestedTime = (2 / 100) * estimator
+    .severeImpact
+    .infectionsByRequestedTime;
+
+  estimator.severeImpact.dollarsInFlight = estimator.severeImpact.infectionsByRequestedTime * data
+    .region.avgDailyIncomePopulation * data
+    .region.avgDailyIncomeInUSD * data.timeToElapse;
+  estimator.severeImpact.dollarsInFlight = 0;
+  return estimator;
+};
 
 export default covid19ImpactEstimator;
